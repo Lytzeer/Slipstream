7c3d499e40446173d37e68261fb88a2c5290e4aa
Lytzeer <lukasportier@gmail.com>
2026-03-01 10:14:14 +0100

    Refactor TabLayout to use NativeTabs.Trigger components for labels and icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 
-import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
+import { NativeTabs } from "expo-router/unstable-native-tabs";
 
 export default function TabLayout() {
   return (
     <NativeTabs>
       <NativeTabs.Trigger name="index">
-        <Label>Home</Label>
-        <Icon sf="house.fill" drawable="custom_android_drawable" />
+        <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon
+          sf="house.fill"
+          drawable="custom_android_drawable"
+        />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="explore">
-        <Icon sf="paperplane.fill" drawable="custom_settings_drawable" />
-        <Label>Settings</Label>
+        <NativeTabs.Trigger.Icon
+          sf="paperplane.fill"
+          drawable="custom_settings_drawable"
+        />
+        <NativeTabs.Trigger.Label>Settings</NativeTabs.Trigger.Label>
       </NativeTabs.Trigger>
     </NativeTabs>
   );
