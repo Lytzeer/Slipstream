9ff0c4db26b2439f4597e5cef151c2f31d6ad70d
Gabrielle Baquié <117845004+GabsBaquie@users.noreply.github.com>
2026-03-05 15:47:37 +0100

    Refactor TabLayout to simplify label and icon usage with direct imports from expo-router

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,23 +1,17 @@
 import React from "react";
 
-import { NativeTabs } from "expo-router/unstable-native-tabs";
+import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
 
 export default function TabLayout() {
   return (
     <NativeTabs>
       <NativeTabs.Trigger name="index">
-        <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon
-          sf="house.fill"
-          drawable="custom_android_drawable"
-        />
+        <Label>Home</Label>
+        <Icon sf="house.fill" drawable="custom_android_drawable" />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="explore">
-        <NativeTabs.Trigger.Icon
-          sf="paperplane.fill"
-          drawable="custom_settings_drawable"
-        />
-        <NativeTabs.Trigger.Label>Settings</NativeTabs.Trigger.Label>
+        <Icon sf="paperplane.fill" drawable="custom_settings_drawable" />
+        <Label>Settings</Label>
       </NativeTabs.Trigger>
     </NativeTabs>
   );
