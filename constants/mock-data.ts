9551d291e67bb85c9971ca723730c41a2e7c201e
Lytzeer <lukasportier@gmail.com>
2026-03-30 21:36:00 +0200

    Add news screen with filtering and search functionality; implement NewsArticleCard component and localization

--- constants/mock-data.ts
+++ constants/mock-data.ts
@@ -1,4 +1,4 @@
-import type { CalendarRace, ChampionshipRaw } from "@/types";
+import type { CalendarRace, ChampionshipRaw, NewsArticle } from "@/types";
 
 export const championshipsList: ChampionshipRaw[] = [
   { id: "ELMS", nameKey: "championships.ELMS", color: "#FF3B31" },
@@ -63,6 +63,57 @@ export const calendarRaces: CalendarRace[] = [
     date: "22 Février 2026",
     time: "14:00",
     completed: true,
+  },
+];
+
+export const newsArticles: NewsArticle[] = [
+  {
+    id: "1",
+    titleKey: "news.article1Title",
+    championshipId: "ELMS",
+    categoryKey: "news.categoryAnalysis",
+    date: "24 Mar 2026",
+    readTimeKey: "news.article1ReadTime",
+  },
+  {
+    id: "2",
+    titleKey: "news.article2Title",
+    championshipId: "LMC",
+    categoryKey: "news.categoryResult",
+    date: "22 Mar 2026",
+    readTimeKey: "news.article2ReadTime",
+  },
+  {
+    id: "3",
+    titleKey: "news.article3Title",
+    championshipId: "GTWORLD",
+    categoryKey: "news.categoryPreview",
+    date: "20 Mar 2026",
+    readTimeKey: "news.article3ReadTime",
+  },
+  {
+    id: "4",
+    titleKey: "news.article4Title",
+    championshipId: "ELMS",
+    categoryKey: "news.categoryInterview",
+    date: "18 Mar 2026",
+    readTimeKey: "news.article4ReadTime",
+  },
+  {
+    id: "5",
+    titleKey: "news.article5Title",
+    championshipId: "LMC",
+    categoryKey: "news.categoryAnalysis",
+    date: "15 Mar 2026",
+    readTimeKey: "news.article5ReadTime",
+  },
+  {
+    id: "6",
+    titleKey: "news.article6Title",
+    championshipId: "GTWORLD",
+    categoryKey: "news.categoryResult",
+    date: "12 Mar 2026",
+    readTimeKey: "news.article6ReadTime",
   },
 ];
 
--- types/index.ts
+++ types/index.ts
@@ -11,3 +11,12 @@ export type CalendarRace = {
   time: string;
   completed: boolean;
 };
+
+export type NewsArticle = {
+  id: string;
+  titleKey: string;
+  championshipId: string;
+  categoryKey: string;
+  date: string;
+  readTimeKey: string;
+};
